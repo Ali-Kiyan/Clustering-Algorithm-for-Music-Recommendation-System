028fcd519e69a46deb2da6118d7d352662ad62c1
Ali <alikiyand@gmail.com>
2017-11-30 21:33:01 +0000

    adding csv export

--- MusicFeatureAnalysis.R
+++ MusicFeatureAnalysis.R
@@ -9,25 +9,12 @@ head(music_cleaned)
 #getting the pair of clusters
 pairs(music_cleaned)
 #scatter plot for a sample attribute
-plot(music_cleaned$song.hotttnesss~ music_cleaned$artist.hotttnesss, data= music_cleaned)
+plot(music_cleaned$song.hotttnesss~ music_cleaned$tempo, data= music_cleaned)
 #adding the terms(genre) --a lot of calculation
 with(music_cleaned,text(music_cleaned$song.hotttnesss~ music_cleaned$artist.hotttnesss, labels=music_cleaned$terms,pos=4,cex=.6))
 #removing the nominal value
 music_cleaned <- music_cleaned[,-c(11)]
 
-#getting the cols of the dataset
-
-artist_hotness <- music_cleaned$artist.hotttness
-bars_start <- music_cleaned$bars_start
-beats_start <- music_cleaned$beats_start
-duration <- music_cleaned$duration
-start_of_fade_out <- music_cleaned$start_of_fade_out
-end_of_fade_out <- music_cleaned$end_of_fade_in
-loudness <- music_cleaned$loudness
-song_hotness <- music_cleaned$song.hotttnesss
-tatums_start <- music_cleaned$tatums_start
-tempo <- music_cleaned$tempo
-time_signature <- music_cleaned$time_signature
 #normalizing with range between -1 and 1
 col <- ncol(music_cleaned)
 newmin = -1
@@ -42,31 +29,36 @@ for(j in 1:col)
   current <- music_cleaned[j]
   temp <- ((current-oldmin)/(oldmax-oldmin))*(newmax-newmin) + newmin
   temp <- as.data.frame(temp)
-  nrml_music <- cbind(x, data.frame(temp))
+  nrml_music <- cbind(nrml_music, data.frame(temp))
 }
 nrml_music <- nrml_music[,-c(1)]
 
-dim(music_cleaned)
-newmin = -1
-newmax= 1
-oldmin <- min(tempo)
-oldmax <- max(tempo)
-newtempo <- ((tempo-oldmin)/(oldmax-oldmin))*(newmax-newmin) + newmin
-newtempo <- as.data.frame(newtempo)
-newtempo <- cbind(newtempo,data.frame(hotness))
-newtempo <- newtempo[complete.cases(newtempo),]
-nrml_music <- newtempo
+#standardisation with substraction of mean and division by standard deviation with scale function (as an alternative to normalization)
 means = apply(music_cleaned, 2, mean)
 sds = apply(music_cleaned, 2, sd)
 nrml_music <- scale(music_cleaned, center = means, scale=sds)
 nrml_music <- as.data.frame(nrml_music)  
+#calculating the distance matrix
 distance = dist(nrml_music)
-distance
+
+
 kc <- kmeans(nrml_music,4)
-kc
+
+#center of each cluster
 kc$centers
-table(music_cleaned$song.hotttnesss, kc$cluster)
-plot(music_cleaned$tempo ~ music_cleaned$song.hotttnesss, data = music_cleaned,col=kc$cluster)
-plot(music_cleaned[c("song.hotttnesss", "artist.hotttnesss")], col=kc$cluster)
-plot(music_cleaned[c("song.hotttnesss", "tempo")], col=nrml_music$song.hotttnesss)
-plot(music_cleaned[c("song.hotttnesss", "tempo")], col=nrml_music$end_of_fade_in)
+#distribution of terms(genre in clusters)
+table(music_cleaned$tempo, kc$cluster)
+
+plot(music_cleaned$song.hotttnesss ~ music_cleaned$artist.hotttnesss, data = music_cleaned,col=kc$cluster)
+plot(music_cleaned[c("artist.hotttnesss", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("bars_start", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("beats_start", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("duration", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("end_of_fade_in", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("loudness", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("start_of_fade_out", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("tatums_start", "song.hotttnesss")], col=kc$cluster)
+plot(music_cleaned[c("song.hotttnesss", "tempo")], col=kc$cluster)
+plot(music_cleaned[c("tempo", "song.hotttnesss")], col=kc$cluster)
+#plot(music_cleaned[c("time_signature", "song.hotttnesss")], col=kc$cluster)
+
